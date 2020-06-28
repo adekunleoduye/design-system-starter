1e301ee571666607372cc421459a0ddb9000346a
Adekunle Oduye <adekunleoduye@gmail.com>
2020-06-28 18:45:35 -0400

    Docs: Updated layout and tokens

--- documentation/src/templates/basicLayout.js
+++ documentation/src/templates/basicLayout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
+import SEO from 'components/seo';
 import {MDXRenderer}  from 'gatsby-plugin-mdx';
 
 const BasicLayout = ({ data }) => {
     return (
       <Layout>
+        <SEO title={data.mdx.frontmatter.title} />
         <h1>{data.mdx.frontmatter.title}</h1>
         <MDXRenderer>
           {data.mdx.body}
